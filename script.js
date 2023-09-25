a7d85a03ef1196007311e7df4f9b525b82af3886
Ioannis Antoniou <johnantony24@hotmail.com>
2023-09-25 08:55:35 +0200

    implement game logic

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
+//querySelector
+/*document.querySelector takes a parent class/id or child and you can manipulate it.
+document.querySelector('.message').textContent = 'Correct Number!';*/
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+//event listener
+/* Event listener is something that happens on the page,
+like a click,
+hover,
+pressing a key,
+scrolling down
+or resizing the window*/
 
-document.querySelector('.guess').value = 23;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+
+    //winning condition
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //too high
+  } else if (guess > secretNumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //too low
+  } else if (guess < secretNumber) {
+    if(score > 1)
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+  } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+  }
+
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+});
